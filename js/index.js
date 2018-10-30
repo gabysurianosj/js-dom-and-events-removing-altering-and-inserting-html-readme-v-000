dba48c11a95750d006afac0f0747e0341edbdf1b
Gabriela Suriano <surianogabriela@gmail.com>
2018-10-30 16:58:51 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -4,14 +4,20 @@ var element = document.createElement('div');
 element.innerHTML = 'Hello, DOM!';
 element.style.backgroundColor = '#f9f9f9';
 element.style.textAlign = 'center';
- document.body.appendChild(element);
- var ul = document.createElement('ul');
+
+document.body.appendChild(element);
+
+var ul = document.createElement('ul');
  for (let i = 0; i < 3; i++) {
   let li = document.createElement('li');
   li.innerHTML = (i + 1).toString();
   ul.appendChild(li);
-}
- element.appendChild(ul);
- ul.style.textAlign = 'left';
- ul.removeChild(ul.querySelector('li:nth-child(2)'));
- ul.remove();
+  }
+
+element.appendChild(ul);
+
+ul.style.textAlign = 'left';
+
+ul.removeChild(ul.querySelector('li:nth-child(2)'));
+
+ul.remove();
